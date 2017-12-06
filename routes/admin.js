8fdf8573d0a51ce0a4f73e899297fe786fd7802f
peiwen <jing1992210@163.com>
2017-12-07 00:55:12 +0800

    ammend

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,16 @@ var router = express.Router();
 
 var sess;
 
+function get_line(whole_line){
+    var k = whole_line.split('#');
+    return k;  
+}
+
 router.get('/login', function(req, res, next){
-
-
 
   sess = req.session;
   if(sess.authenticated){
-    return res.redirect('/admin');
+    return res.redirect('/admin/index');
   }
   else{
     return res.render('admin/login');
@@ -50,26 +53,7 @@ router.post('/login', function(req,res, next){
 router.get('/index', function (req, res) {
    sess = req.session;
    if( sess.authenticated ){
-    CardModel.find({}, function(err, cards){
-      var context = {
-        cards : cards.map(function(card){
-          return{
-            packageName : card.packageName,
-            packagePrice : card.package_price,
-            cardType : card.cardType,
-            rightItem : card.rightItem,
-            expression : card.expression,
-            blueItem : card.blueItem,
-            redItem : card.redItem,
-            firstLine : card.firstLine,
-            lastLine : card.lastLine,
-            analysis : card.analysis,
-            card_unique_id : card.card_unique_id
-          }
-        })
-      }   
-      res.render('admin/index', context); 
-    });
+    return res.render('admin/index'); 
    }
    else{
       return res.redirect('/admin/login');
@@ -103,7 +87,7 @@ router.post('/index', function(req, res){
 
       while(i < jsonArrayobj.length){
         if(jsonArrayobj[i].hasOwnProperty('whole_line')){
-          var k = jsonArrayobj[i].whole_line.split('#');
+          var k = get_line(jsonArrayobj[i]);
           var firstLine = k[0];
           var lastLine = k[1];      
           var packagename = cardFile.name.split(".")[0];
@@ -147,6 +131,70 @@ router.post('/index', function(req, res){
     });
 
     return res.render('admin/index')
+});
+
+router.get('/index/search', function(req, res){
+    var search_key = req.query.searchKey;
+    CardModel.find({card_unique_id : search_key}, function(err, cards){
+      var context = {
+        cards : cards.map(function(card){
+          return{
+            packageName : card.packageName,
+            packagePrice : card.package_price,
+            cardType : card.cardType,
+            rightItem : card.rightItem,
+            expression : card.expression,
+            blueItem : card.blueItem,
+            redItem : card.redItem,
+            firstLine : card.firstLine,
+            lastLine : card.lastLine,
+            analysis : card.analysis,
+            card_unique_id : card.card_unique_id
+          }
+        })
+      }
+      return res.render('admin/index', context); 
+    });
+});
+
+router.get('/index/update', function(req, res){
+
+    var card_unique_id = req.query.card_unique_id;
+    var blueItem = req.query.blueItem;
+    var redItem = req.query.redItem;
+    var packageName = req.query.packageName;
+    var analysis = req.query.analysis;
+    var expression = req.query.expression;
+    var rightItem = req.query.rightItem;
+    var whole_line = req.query.whole_line;
+
+    var k = get_line(whole_line);
+
+    var data_json = {
+      'rightItem' : req.query.rightItem,
+      'expression' : req.query.expression,
+      'blueItem' : req.query.blueItem,
+      'redItem' : req.query.redItem,
+      'firstLine' : k[0],
+      'lastLine' : k[1],
+      'analysis' : req.query.analysis
+    };
+
+    CardModel.find({card_unique_id : card_unique_id}, function(err, cards){
+    if(cards.length === 0){
+      return res.render('admin/index');
+    }
+    else{
+      var _id = cards[0]._id;
+      CardModel.findByIdAndUpdate(_id, { $set: data_json}, {new: true}, function(err, cards){
+        if (err) return handleError(err);        
+      });
+    }
+
+    return res.render('admin/index');
+
+    });
+
 });
 
 module.exports = router;
