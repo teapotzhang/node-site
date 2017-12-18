6ee5fce2de6f73f2407122d7b4802cfac74c8c73
peiwen <jing1992210@163.com>
2017-12-18 18:03:57 +0800

    no idea promise

--- routes/get_card.js
+++ routes/get_card.js
@@ -57,7 +57,7 @@ function getNextCard(openID, cb){
       activated : true
     };
 
-    UserCardModel.findOne(query, null, {sort: {LastShowDate: -1}}, function(err, user_card){
+    UserCardModel.findOne(query, null, {sort: {LastShowDate: -1}}, function(err, user_card) {
     if( user_card == null )
     {
       //当天没有可以刷的卡了
@@ -131,6 +131,11 @@ router.get('/', function(req, res, next){
 
       var tag;
 
+      console.log('answer we get seconds    ' + seconds);
+      console.log('answer we get answerStatus      ' + answerStatus);
+      console.log('answer we get card_unique_id     ' + card_unique_id);
+      console.log('answer we get sessionID      ' + sessionID);
+
       if( answerStatus == 'false' ){
         tag = 3; //回答错了
       } 
@@ -143,6 +148,8 @@ router.get('/', function(req, res, next){
         }
 
       }
+
+      console.log('answer we get tag     ' + tag);
 
       UserCardModel.find({'card_unique_id' : card_unique_id}, function(err, cards){
         //更新LastShowDate, LastUpdateDate和usedStatus
