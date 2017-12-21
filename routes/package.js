b2442c3ba5c23e615a40272a7ede2a88923b76c9
peiwen <jing1992210@163.com>
2017-12-21 13:47:59 +0800

    implement wx pay

--- routes/package.js
+++ routes/package.js
@@ -26,7 +26,7 @@ router.get('/', function(req, res, next){
               }
             })
           };
-          console.log(content);
+          console.log(context);
           res.json(context);
       });
     });
