6b0bf293fc7a61a5989840035781a0132515598f
peiwen <jing1992210@163.com>
2017-12-26 05:53:36 +0800

    更改包价格

--- routes/order.js
+++ routes/order.js
@@ -86,14 +86,14 @@ router.get('/', function(req, res, next){
         var body =  '<xml>' + 
                     '<appid>wxf965e072652b2dc6</appid>' + 
                     '<mch_id>1492751112</mch_id>' +
-                    '<nonce_str><![CDATA[' + nonce_str + ']]></nonce_str>' +
-                    '<body><![CDATA[' + '法考小卡片-' + packageName + ']]></body>' +
-                    '<notify_url><![CDATA[https://jiyikapian.com/order/notify]]></notify_url>' +
-                    '<out_trade_no><![CDATA[' + orderID + ']]></out_trade_no>' +
-                    '<spbill_create_ip><![CDATA[140.143.136.128]]></spbill_create_ip>' +
+                    '<nonce_str>' + nonce_str + '</nonce_str>' +
+                    '<body>' + 'test' + '</body>' +
+                    '<notify_url>https://jiyikapian.com/order/notify</notify_url>' +
+                    '<out_trade_no>' + orderID + '</out_trade_no>' +
+                    '<spbill_create_ip><![CDATA[140.143.136.128</spbill_create_ip>' +
                     '<total_fee>' + packagePrice + '</<total_fee>' +
                     '<trade_type>JSAPI</trade_type>' +
-                    '<sign><![CDATA[' + sign + ']]></sign>' +
+                    '<sign>' + sign + '</sign>' +
                     '</xml>';
         console.log(body);
 
