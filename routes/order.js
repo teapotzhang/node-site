194b38f72e960b9298b5015640901366d84341d0
peiwen <jing1992210@163.com>
2017-12-26 05:35:55 +0800

    更改包价格

--- routes/order.js
+++ routes/order.js
@@ -86,13 +86,14 @@ router.get('/', function(req, res, next){
         var body =  '<xml>' + 
                     '<appid>' + 'wxf965e072652b2dc6' + '</appid>' + 
                     '<mch_id>' + '1492751112' + '</mch_id>' +
+                    '<nonce_str><![CDATA[' + nonce_str + ']]></nonce_str>' +
                     '<body>' + '法考小卡片-' + packageName + '</body>' +
                     '<notify_url>' + 'https://jiyikapian.com/order/notify' + '</notify_url>' +
-                    '<out_trade_no>' + orderID + '</out_trade_no>' +
-                    '<spbill_create_ip>' + '140.143.136.128' + '</spbill_create_ip>' +
+                    '<out_trade_no>' + orderID + ']]></out_trade_no>' +
+                    '<spbill_create_ip><![CDATA[140.143.136.128]]></spbill_create_ip>' +
                     '<total_fee>' + packagePrice + '</<total_fee>' +
                     '<trade_type>' + 'JSAPI' + '</trade_type>' +
-                    '<sign>' + sign + '</sign>' +
+                    '<sign><![CDATA[' + sign + ']]></sign>' +
                     '</xml>';
         console.log(body);
 
