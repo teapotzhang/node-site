64a41936734aa6c43c4c9a9505d50bccd4d1518f
peiwen <jing1992210@163.com>
2017-12-28 19:12:40 +0800

    add random

--- routes/get_card.js
+++ routes/get_card.js
@@ -38,7 +38,7 @@ function addDays(date, days) {
   return result;
 }
 
-function getNextCard(openID, cb){
+function getNextCard(openID, card_flag, cb){
     var today_obj = new Date();
     var today_num = dateObjToDateNumber(today_obj);
     var tomorrow = addDays( today_num, 1 );
@@ -73,12 +73,7 @@ function getNextCard(openID, cb){
       Showed : false
     };
 
-    UserCardModel.findOne(query_showed_cards, null, {sort: {LastShowDate: -1}}, function(err, user_card) {
-    console.log(user_card);
-    if( user_card == null )
-    {
-      //所有的卡都是新卡，或者当天没有可以刷的卡了
-      //随机找一张没有出现过的卡片
+    if( card_flag ){
       UserCardModel.findOneRandom(query_unshowed_cards, null, {sort: {LastShowDate: -1}}, function(err, new_user_card) {
         console.log(new_user_card);
         if( new_user_card == null ){
@@ -108,40 +103,87 @@ function getNextCard(openID, cb){
               reelNumber : card['reelNumber'],
               questionNumber : card['questionNumber'],
               card_unique_id : card_unique_id,
+              new_card : true,
               lastCard : false     
             }
             var get_json = JSON.stringify(card_json);
             cb(get_json);
           });          
         }
-      });
-    }
-    else
-    {
-      card_unique_id = user_card.card_unique_id;
-      CardModel.findOne({'card_unique_id': card_unique_id}, function(err, card){
-        var json = JSON.stringify(card)
-        card_json = {
-          packageName: card['packageName'],
-          packageType: card['cardType'],
-          firstLine: card['firstLine'],
-          lastLine: card['lastLine'],
-          blueItem: card['blueItem'],
-          redItem: card['redItem'],
-          blueRight: card['rightItem'] % 2,
-          analysis: card['analysis'],
-          expression : card['expression'],
-          yearNumber : card['yearNumber'],
-          reelNumber : card['reelNumber'],
-          questionNumber : card['questionNumber'],
-          card_unique_id : card_unique_id,
-          lastCard : false     
-        }
-        var get_json = JSON.stringify(card_json);
-        cb(get_json);
       });      
     }
-  });
+    else{
+      UserCardModel.findOne(query_showed_cards, null, {sort: {LastShowDate: -1}}, function(err, user_card) {
+      console.log(user_card);
+      if( user_card == null )
+      {
+        //所有的卡都是新卡，或者当天没有可以刷的卡了
+        //随机找一张没有出现过的卡片
+        UserCardModel.findOneRandom(query_unshowed_cards, null, {sort: {LastShowDate: -1}}, function(err, new_user_card) {
+          console.log(new_user_card);
+          if( new_user_card == null ){
+            //完全没卡能刷
+            card_json = {
+              lastCard: true
+            }
+            var get_json = JSON.stringify(card_json);
+            cb(get_json);
+          }
+          else{
+            //有没刷过的新卡哦
+            card_unique_id = new_user_card.card_unique_id;
+            CardModel.findOne({'card_unique_id': card_unique_id}, function(err, card){
+              var json = JSON.stringify(card)
+              card_json = {
+                packageName: card['packageName'],
+                packageType: card['cardType'],
+                firstLine: card['firstLine'],
+                lastLine: card['lastLine'],
+                blueItem: card['blueItem'],
+                redItem: card['redItem'],
+                blueRight: card['rightItem'] % 2,
+                analysis: card['analysis'],
+                expression : card['expression'],
+                yearNumber : card['yearNumber'],
+                reelNumber : card['reelNumber'],
+                questionNumber : card['questionNumber'],
+                card_unique_id : card_unique_id,
+                new_card : false,
+                lastCard : false     
+              }
+              var get_json = JSON.stringify(card_json);
+              cb(get_json);
+            });          
+          }
+        });
+      }
+      else
+      {
+        card_unique_id = user_card.card_unique_id;
+        CardModel.findOne({'card_unique_id': card_unique_id}, function(err, card){
+          var json = JSON.stringify(card)
+          card_json = {
+            packageName: card['packageName'],
+            packageType: card['cardType'],
+            firstLine: card['firstLine'],
+            lastLine: card['lastLine'],
+            blueItem: card['blueItem'],
+            redItem: card['redItem'],
+            blueRight: card['rightItem'] % 2,
+            analysis: card['analysis'],
+            expression : card['expression'],
+            yearNumber : card['yearNumber'],
+            reelNumber : card['reelNumber'],
+            questionNumber : card['questionNumber'],
+            card_unique_id : card_unique_id,
+            lastCard : false     
+          }
+          var get_json = JSON.stringify(card_json);
+          cb(get_json);
+        });      
+      }
+    });
+  }
 }
 
 router.get('/', function(req, res, next){
@@ -161,7 +203,7 @@ router.get('/', function(req, res, next){
       var card_json;
 
       var PromiseGetNextCard = new Promise(function(resolve,reject){
-        getNextCard(openID, function(result){
+        getNextCard(openID, false, function(result){
           resolve(result);
          });
       });
@@ -177,6 +219,7 @@ router.get('/', function(req, res, next){
       var seconds = parseInt(req.query.seconds);
       var answerStatus = req.query.answerStatus;
       var card_unique_id = req.query.card_unique_id;
+      var new_card = req.query.new_card;
       
 
       var tag;
@@ -259,7 +302,7 @@ router.get('/', function(req, res, next){
             var card_json;
 
             var PromiseGetNextCard = new Promise(function(resolve,reject){
-              getNextCard(openID, function(result){
+              getNextCard(openID, new_card, function(result){
                 resolve(result);
                });
             });
