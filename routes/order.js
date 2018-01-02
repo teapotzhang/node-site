16fca9cf52a92b6fc5745668924abbda8ec70467
peiwen <jing1992210@163.com>
2018-01-02 20:37:24 +0800

    cry

--- routes/order.js
+++ routes/order.js
@@ -192,18 +192,18 @@ router.get('/download', function(req, res, next){
 
 router.get('/query', function(req, res, next){
   wxpay.queryOrder({
-    out_trade_no : '20180102827312'
+    out_trade_no : '20180102477060'
   }, function(err, data){
     console.log(data);
 
     wxpay.queryRefundOrder({
-      out_trade_no : '20180102827312'
+      out_trade_no : '20180102477060'
     }, function(err, data){
       console.log(data);
 
 
         wxpay.queryRefundOrder({
-          out_trade_no : '20180102827312'
+          out_trade_no : '20180102477060'
         }, function(err, data){
           console.log(data);
         });
