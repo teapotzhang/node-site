e2293e761d8f4b3b793261d82e5869d7898b152f
peiwen <jing1992210@163.com>
2018-01-10 16:22:11 +0800

    make sure intro at first

--- routes/get_card.js
+++ routes/get_card.js
@@ -59,7 +59,7 @@ function getNextCard(openID){
       activated : true
     };
     
-    UserCardModel.find(query, null, {limit:100}, {sort: {LastShowDate: -1, randomNumber: 1}}, function(err, new_user_cards) {
+    UserCardModel.find(query, null, {limit:100, sort: {LastShowDate: -1, randomNumber: 1}}, function(err, new_user_cards) {
       console.log(new_user_cards);
       if( new_user_cards.length == 0 ){
         //完全没卡能刷
