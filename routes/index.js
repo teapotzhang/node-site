70601c094e0b3f3831da0603977a98b8db6bb712
peiwen <jing1992210@163.com>
2018-03-30 15:05:00 +0800

    change back to only number

--- routes/index.js
+++ routes/index.js
@@ -151,11 +151,10 @@ router.get('/getTotalArray', function(req, res, next){
   var total_array = [];
   UserModel.aggregate({$sample:{size:1500}}, function(err, users){
     async.each(users, function(user, callback){
-      total_array.push(parseInt(user['totalCards']));
+      total_array.push(user['totalCards']);
       callback();
     }, function(results){
-      total_array = JSON.stringify(total_array);
-      res.json(total_array);
+      res.json({'array':total_array});
     });
   });
 });
