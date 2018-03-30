3885e4b2a735d235c5fa18f98573b0bae0adf422
peiwen <jing1992210@163.com>
2018-03-30 16:10:28 +0800

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -158,7 +158,6 @@ router.get('/getTotalArray', function(req, res, next){
     for(let i = 0;i < size; i += interval){
         resultData.push(datas[i]['totalCards'])
     }
-    resultData.slice(0, 1500);
     res.json({'array':resultData});
   });
 
